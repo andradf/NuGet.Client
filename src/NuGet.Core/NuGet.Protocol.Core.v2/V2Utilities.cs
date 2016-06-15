fed70b8b218256634efda1d7e7708b215eb68447
Maarten Balliauw <maarten@maartenballiauw.be>
2016-06-15 17:23:34 +0200

    Added check for accept-language string

--- src/NuGet.Core/NuGet.Protocol.Core.v2/V2Utilities.cs
+++ src/NuGet.Core/NuGet.Protocol.Core.v2/V2Utilities.cs
@@ -44,7 +44,8 @@ namespace NuGet.Protocol.Core.v2
                         {
                             httpReq.UserAgent = _userAgent;
 
-                            if (httpReq.Headers.Get(AcceptLanguage.HeaderName)?.Length == 0)
+                            if (httpReq.Headers.Get(AcceptLanguage.HeaderName)?.Length == 0 
+                                && !string.IsNullOrEmpty(acceptLanguage))
                             {
                                 httpReq.Headers.Add(AcceptLanguage.HeaderName, acceptLanguage);
                             }
